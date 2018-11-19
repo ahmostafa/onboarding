df0194b638dff5252878f464d7cb549feebee501
Ahmed Mostafa <engahmed.mostafa@yahoo.com>
2018-11-20 02:35:48 +0300

    Adding error in load of inappbrowser but with alert not page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { SlidesActionsMap } from './../../serverconnect/slideactionmap';
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { NavController,Slides } from 'ionic-angular';
+import { NavController,Slides, Platform } from 'ionic-angular';
 import { OnBoardingSlide } from '../../classes/onboardingslide';
 import { OnBoardingProvider } from '../../providers/on-boarding/on-boarding';
 import { WelcomescreenPage } from '../welcomescreen/welcomescreen';
 import { NativePageTransitions,NativeTransitionOptions } from '@ionic-native/native-page-transitions';
 import {Storage} from '@ionic/storage';
+import { Subscription } from 'rxjs/Subscription';
 // import { providerDef } from '@angular/core/src/view';
 
 @Component({
@@ -21,7 +22,7 @@ export class HomePage implements OnInit {
   isLastSlide:boolean=false;
   lastSlideIndexBeforeLeaveValue = -1;
   lastSlideIndexBeforeLeaveKey = "slideindex";
-
+  onPausSubscription: Subscription;
    transitionOptions : NativeTransitionOptions = {
     duration:400,
     androiddelay:0,
@@ -30,11 +31,19 @@ export class HomePage implements OnInit {
     
   }
   constructor(public navCtrl: NavController,private nativePageTransitions: NativePageTransitions,
-    private onBoardingProvider: OnBoardingProvider, private inAppBrowser: InAppBrowser, private storage: Storage) {
+    private onBoardingProvider: OnBoardingProvider, private inAppBrowser: InAppBrowser, 
+    private storage: Storage, private platform:Platform) {
 
       this.storage.get(this.lastSlideIndexBeforeLeaveKey).then(index=>{
         this.lastSlideIndexBeforeLeaveValue=index?index:-1;
         
+      });
+      this.onPausSubscription = this.platform.pause.subscribe(()=>{
+        if(!this.lastSlideIndexBeforeLeaveValue || this.lastSlideIndexBeforeLeaveValue===-1){
+          console.log(`lastSlideIndexBeforeLeaveKey ${this.lastSlideIndexBeforeLeaveKey} index ${this.slides.getActiveIndex()} `)
+          this.storage.set(this.lastSlideIndexBeforeLeaveKey,this.slides.getActiveIndex())
+          
+        }
       });
   }
 
@@ -47,15 +56,17 @@ export class HomePage implements OnInit {
     } ,
       errMsg => this.errMsg = errMsg);
   }
-  
+  ngOnDestroy(): void {
+    this.onPausSubscription.unsubscribe();
+  }
   slideChanged(){
     this.isHidePrevButton = this.slides.isBeginning();
     this.isLastSlide = this.slides.isEnd();// because this is called after the slide already change  //&&this.slides.getActiveIndex()=== (this.slides.length()-1)
     console.log('this.lastSlideIndexBeforeLeaveValue '+this.lastSlideIndexBeforeLeaveValue);
-    if(!this.lastSlideIndexBeforeLeaveValue || this.lastSlideIndexBeforeLeaveValue===-1){
-      this.storage.set(this.lastSlideIndexBeforeLeaveKey,this.slides.getActiveIndex())
-      console.log(`lastSlideIndexBeforeLeaveKey ${this.lastSlideIndexBeforeLeaveKey} index ${this.slides.getActiveIndex()} `)
-    }
+    // if(!this.lastSlideIndexBeforeLeaveValue || this.lastSlideIndexBeforeLeaveValue===-1){
+    //   this.storage.set(this.lastSlideIndexBeforeLeaveKey,this.slides.getActiveIndex())
+    //   console.log(`lastSlideIndexBeforeLeaveKey ${this.lastSlideIndexBeforeLeaveKey} index ${this.slides.getActiveIndex()} `)
+    // }
   }
   prevSlide(){
     console.log('prev')
@@ -78,9 +89,44 @@ export class HomePage implements OnInit {
     return SlidesActionsMap.has(identifier);
   }
   openActionUrl(identifier:string){
-    this.inAppBrowser.create(SlidesActionsMap.get(identifier),'_blank').show();
+    let inAppBrowserRef=this.inAppBrowser.create(SlidesActionsMap.get(identifier),'_blank');
+    inAppBrowserRef.show();
+    inAppBrowserRef.on('loaderror').subscribe(event=>{
+      inAppBrowserRef.executeScript({code:"alert('Error in load:"+SlidesActionsMap.get(identifier) +" "+event.message +" ');"})
+      // inAppBrowserRef.executeScript({code:"document.body.innerHTML+='<div style='position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;'>"+event.message+"</div>'"})
+    })
+    // inAppBrowserRef.on('loaderror').subscribe(event=>{
+    //   let scriptErrorMesssage = this.createMsg('Sorry we cannot open that page. Message from the server is: '
+    //   + event.message);
+    //   inAppBrowserRef.executeScript({code:scriptErrorMesssage});
+    // })
+   // inAppBrowserRef.show();
+    
+    
     console.log(SlidesActionsMap.get(identifier));
   }
+
+   createMsg(msg) {
+    return 'var div = document.createElement("div");'
+         + 'div.style.position = "absolute";'
+         + 'div.style.top = "50%";'
+         + 'div.style.left = "50%";'
+         + 'div.style.width = "100px";'
+         + 'div.style.height = "100px";'
+         + 'div.style.color = "#f00";'
+         + 'div.innerHTML = "' + msg + '";'
+         + 'document.appendChild(div);'
+  }
+//   executeScriptCallBack(params) {
+
+//     if (params[0] == null) {
+
+//         $('#status-message').text(
+//            "Sorry we couldn't open that page. Message from the server is : '"
+//            + params.message + "'");
+//     }
+
+// }
   goToLastSlide(){
     setTimeout(() => {
       this.slides.slideTo(this.lastSlideIndexBeforeLeaveValue); 
